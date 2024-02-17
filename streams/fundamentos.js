487bd8deed42547714c541ffedff83265d4f2056
italomoraess <italomoraes.sth@gmail.com>
2024-02-17 20:33:08 -0300

    Entendendo strams de escrita e transformação

--- streams/fundamentos.js
+++ streams/fundamentos.js
@@ -1,5 +1,7 @@
-import { Readable } from 'node:stream'
+import { error } from 'node:console';
+import { Readable, Writable, Transform } from 'node:stream'
 
+//Stream de leitura
 class OneToHoundredStream extends Readable {
   index = 1;
   
@@ -18,4 +20,23 @@ class OneToHoundredStream extends Readable {
   }
 }
 
-new OneToHoundredStream().pipe(process.stdout)
+//Stream de escrita
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10)
+    callback()
+  }
+}
+
+//Stream de transformação
+class InverseNumberToNegative extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1
+
+    callback(null, Buffer.from(String(transformed)))
+  }
+}
+
+new OneToHoundredStream()
+  .pipe(new InverseNumberToNegative())
+  .pipe(new MultiplyByTenStream())
